9752c1a8c85602f611b0bab7694c47048cfd4a25
incyberz <incyberz@yahoo.com>
2022-12-06 19:03:01 +0700

    gabung-array-example

---
+++ 04-spread-operator/copy-array.js
@@ -0,0 +1,21 @@
+const tim1 = ['ahmad','budi','charlie','deni'];
+
+// ==================================
+// Cara Copy Array yang salah
+// ==================================
+const tim2 = tim1;
+tim2[0] = 'fajar';
+console.log(tim1)
+console.log(tim2)
+console.log(tim1 === tim2)
+
+// ==================================
+// Cara Copy Array dg Rest Operator
+// ==================================
+const tim3 = ['gilang','haris','indra','joni'];
+const tim4 = [...tim3]
+tim4[0] = 'kiki' 
+console.log(tim3)
+console.log(tim4)
+console.log(tim3 === tim4)
+
