4858e6b0e73f2a1667d23bb604badb9c68bb9374
incyberz <incyberz@yahoo.com>
2022-12-06 19:03:28 +0700

    copy-array with spread operator

---
+++ 04-spread-operator/gabung-array.js
@@ -0,0 +1,21 @@
+const buah = ['apel','jeruk','mangga','delima'];
+const sayur = ['bayam','timun'];
+let lainnya = 'daging';
+
+// ==================================
+// Cara Gabung hanya Array
+// ==================================
+const belanjaan2 = buah.concat(sayur);
+console.log(belanjaan2)
+
+// ==================================
+// Cara Gabung yang salah #1
+// ==================================
+const belanjaan = [buah,lainnya,sayur];
+console.log(belanjaan)
+
+// ==================================
+// Cara Gabung dengan Spread Operator
+// ==================================
+const semuaBelanjaan = [...buah,lainnya,...sayur];
+console.log(semuaBelanjaan)
