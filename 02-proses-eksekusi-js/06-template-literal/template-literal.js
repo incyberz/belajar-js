653fe277b9686c9c6ea486f10dac72e749936151
incyberz <incyberz@yahoo.com>
2022-12-09 17:24:12 +0700

    spread operator update

--- 02-proses-eksekusi-js/06-template-literal/template-literal.js
+++ 02-proses-eksekusi-js/06-template-literal/template-literal.js
@@ -1,6 +1,57 @@
 // =============================================
 // STRING LITERAL - MEMAKAI BACKTIC
 // =============================================
-let nama = "Ahmad Firdaus";
-let usia = 20;
-console.log(`Nama saya ${nama}, usia saya ${usia} tahun`);
+// Multi-line String
+// Embedded Expression
+// HTML Fragments
+// Expression Interpolation
+// Tagged Template
+// =============================================
+
+// =============================================
+// Multi-line String
+// =============================================
+// console.log('dengan petik tunggal
+// jika beda baris akan error')
+
+// console.log(`Paragraf ini lebih
+// dari satu baris dengan memakai
+// string literal yang diapit
+// oleh backtic
+// `);
+
+// =============================================
+// Embedded Expression
+// =============================================
+// let nama = "Ahmad Firdaus";
+// let usia = 20;
+// console.log(`Nama saya ${nama}, usia saya ${usia} tahun`);
+
+// =============================================
+// HTML Fragments
+// =============================================
+// let nama = "Ahmad Firdaus";
+// let usia = 20;
+// console.log(`
+// <table class='table'>
+//     <tr>
+//         <td>Nama</td>
+//         <td>${nama}</td>
+//     </tr>
+//     <tr>
+//         <td>Usia</td>
+//         <td>${usia} tahun</td>
+//     </tr>
+// </table>
+// `);
+
+// =============================================
+// Expression Interpolation
+// =============================================
+// let x = 2;
+// let y = 3;
+// console.log(`
+// Jika nilai x = ${x}
+// dan nilai y = ${y}
+// maka hasil perkalian keduanya
+// ${x} * ${y} = ${x * y}`);
---
+++ 04-spread-operator/variable-highlighter.js
@@ -0,0 +1,17 @@
+let nama = "Iin Sholihin";
+let usia = 35;
+let alamat = "Ciwaringin - Cirebon";
+
+function tandai(strings, ...values) {
+  return strings.reduce(
+    (result, str, i) =>
+      `${result}${str}<span class='tandai'>${values[i] || ""}</span>`,
+    ""
+  );
+}
+
+let hasil = tandai`Nama saya ${nama}, usia ${usia} tahun, saya tinggal di ${alamat}`;
+
+console.log(hasil);
+
+document.getElementById("hasil").innerHTML = hasil;
